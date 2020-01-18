bc094225416eb3fec7a65f29cba1ca97e63f8c45
raffizulvian <60030585+raffizulvian@users.noreply.github.com>
2020-01-18 22:36:22 +0700

    Update calc-master.cpp

--- calc-master.cpp
+++ calc-master.cpp
@@ -5,7 +5,7 @@ int main() {
 
     int i, n;
     string opr;
-    
+
     cout << "SELAMAT DATANG\nDi program ini anda bisa melakukan operasi penjumlahan, pengurangan, perkalian, serta penurunan dari polinom\n";
     cout << "Masukkan derajat tertinggi dari polinomial: ";
     cin >> n;
@@ -20,10 +20,26 @@ int main() {
     for (i = 0; i <= n; i++) {
         cout << polA[n-i] << "X^" << n-i << " ";
     }
-    
-    cout << "Silakan pilih operasi yang akan anda lakukan (tambah/kurang/kali/turunan)\n";
+
+    cout << "\nSilakan pilih operasi yang akan anda lakukan (tambah/kurang/kali/turunan)\n";
     cout << "Operasi yang dipilih: ";
     cin >> opr;
+    
+    if (opr == "tambah") {
+        //Masukin sini far
+    }
+    
+    else if (opr == "kurang") {
+        //Masukin sini dro
+    }
+    
+    else if (opr == "kali") {
+        //Masukin sini jul
+    }
+    
+    else if (opr == "turunan") {
+        //
+    }
     
     for (i = 0; i <= n; i++) {
         cout << polA[n-i]*(n-i) << "X^" << (n-i-1) << " ";
@@ -31,3 +47,4 @@ int main() {
 
     return 0;
 }
+
